ce937cc6bc0bc9032d33a2d95905a1523416b7e6
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2020-04-17 02:39:26 +0900

    417
    417

--- matrix/src/App.js
+++ matrix/src/App.js
@@ -10,9 +10,9 @@ function App() {
   return (
     <HashRouter>
       <Navigation />
-      <Route path="/" exact={true} component={Home}></Route>
-      <Route path="/about" component={About}></Route>
-      <Route path="/movie/:id" component={Detail}></Route>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
     </HashRouter>
   );
 }
